f6e0b59cc5570f943d6d4cb6cd44e2bf79f65d2f
perriy8315 <perriy8315@gmail.com>
2025-08-22 19:23:29 +0900

    first project setting

---
+++ qpeek/src/main/java/org/qpeek/qpeek/QpeekApplication.java
@@ -0,0 +1,13 @@
+package org.qpeek.qpeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QpeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QpeekApplication.class, args);
+	}
+
+}
